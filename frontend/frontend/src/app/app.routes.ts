5ea4060fb780f2535f399070fcf0959e04ee3027
cdande <corbyn_anderson@yahoo.com>
2023-10-30 15:16:08 -0400

    Before Break

---
+++ frontend/frontend/src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from "@angular/router";
+import { HomeComponent } from "./pages/home.component";
+
+export const routes: Routes = [
+  {
+    path: "home",
+    component: HomeComponent,
+  },
+
+  {
+    path: "**",
+    redirectTo: "home",
+  },
+];
---
+++ frontend/frontend/src/app/pages/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+  standalone: true,
+  template: `
+    <div class="prose">
+      <h2>Welcome to the Application</h2>
+      <p>This is an application we built in intro to programming</p>
+    </div>
+  `,
+})
+export class HomeComponent {}
