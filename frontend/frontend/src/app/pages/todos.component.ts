7df61ea40d2eddccdc505b3635397621adaceda7
cdande <corbyn_anderson@yahoo.com>
2023-10-30 16:52:06 -0400

    End Of Day

---
+++ frontend/frontend/src/app/pages/todos.component.ts
@@ -0,0 +1,35 @@
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { TodoEntryComponent } from "./components/todo-entry.component";
+import { TodoListComponent } from "./components/todo-list.component";
+import { TodoItem } from "../models";
+
+@Component({
+  selector: "app-todos",
+  standalone: true,
+  template: `
+    <section>
+      <app-todo-entry />
+    </section>
+    <section>
+      <app-todo-list />
+    </section>
+  `,
+  styles: [],
+  imports: [CommonModule, TodoEntryComponent, TodoListComponent],
+})
+export class TodosComponent {
+  list: TodoItem[] = [
+    { id: "1", description: "Wash Car", completed: false },
+    { id: "2", description: "Rake Leaves", completed: true },
+  ];
+
+  addItem(description: string) {
+    const newItem: TodoItem = {
+      id: crypto.randomUUID(),
+      description,
+      completed: false,
+    };
+    this.list = [newItem, ...this.list];
+  }
+}
